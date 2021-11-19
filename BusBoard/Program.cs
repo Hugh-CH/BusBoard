a93dff12cdfa7ea9c1fc576e55f0c8e79cd5ba53
Hugh Hudson <Hugh.Hudson@softwire.com>
2021-11-19 12:22:10 +0000

    initial setup

---
+++ BusBoard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BusBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
