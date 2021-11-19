1679b62de2a496dd90789d4134487a0b15ec8b1a
Hugh Hudson <Hugh.Hudson@softwire.com>
2021-11-19 13:33:37 +0000

    added API reader to prediction class

---
+++ BusBoard/Prediction.cs
@@ -0,0 +1,9 @@
+﻿namespace BusBoard
+{
+    public class Prediction
+    {
+        public string id { get; set; }
+        public string stationName { get; set; }
+        public string timeToStation{ get; set; }
+    }
+}
--- BusBoard/Program.cs
+++ BusBoard/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using RestSharp;
 
 namespace BusBoard
 {
@@ -6,7 +8,16 @@ namespace BusBoard
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new RestClient("https://api.tfl.gov.uk");
+            // client.Authenticator = new HttpBasicAuthenticator("username", "password");
+
+            var request = new RestRequest("StopPoint/490008660N/Arrivals", DataFormat.Json);
+
+
+            //var listOfPredictions = new List<Prediction>();
+            var response = client.Execute<List<Prediction>>(request).Data;
+
+            
         }
     }
 }
